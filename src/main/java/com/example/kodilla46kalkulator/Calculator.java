6cad5195639945e7c5ab9e66d16c2f695c6302bc
Tamara Wala <tamwala@gmail.com>
2022-05-03 18:30:05 +0200

    Second commit

--- src/main/java/com/example/kodilla46kalkulator/Calculator.java
+++ src/main/java/com/example/kodilla46kalkulator/Calculator.java
@@ -1,4 +1,20 @@
 package com.example.kodilla46kalkulator;
 
 public class Calculator {
+
+    public double addition(double a, double b) {
+        return a + b;
+    }
+
+    public double subtraction(double a, double b) {
+        return a - b;
+    }
+
+
+    public static void main(String args[]) {
+        Calculator calculator = new Calculator();
+        calculator.addition(2,5);
+        System.out.println(calculator.addition(2,5));
+
+    }
 }
